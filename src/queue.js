1358a2b1175425608e57b51f80a1ef8902173848
Your Name <you@example.com>
2022-10-17 02:23:14 +0300

    feat: add queue solution

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+//const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,27 +13,62 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+/*class Queue {
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue( value ) {
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   }
 }
+*/
+
+
+
+class Queue {
+  constructor() {
+    this.first = null;
+  }
+  enqueue(value) {
+    let newNode = new ListNode(value)
+    if (!this.first) {
+      this.first = newNode;
+      return
+    }
+      let current = this.first;
+      while (current) {
+        if (!current.next) {
+          current.next = newNode;
+          return;
+        }
+        current = current.next;
+      }
+      current = newNode
+  }
+
+  dequeue() {
+    let current = this.first
+    if (current) {
+      let value = current.value;
+      current = current.next;
+      this.first = current;
+      return value;
+    }
+  }
+
+  getUnderlyingList() {
+    return this.first;
+  }
+}
+
 
 module.exports = {
   Queue
 };
 
 
-//   npm run test test/remove-from-list.test.js
+//   npm run test test/queue.test.js
